3ecc9f91bc5c8077060301e89fb8ad8685a3c28c
Sergey Hayevoy <jantao.dev@gmail.com>
2020-09-15 12:26:18 +0300

    Add unique prefixes to functions

--- gde_zakazy/gde-zakazy.php
+++ gde_zakazy/gde-zakazy.php
@@ -12,8 +12,8 @@
 
 defined('ABSPATH') or die("No script kiddies please!");
 
-if (!function_exists('is_woocommerce_active')) {
-    function is_woocommerce_active() {
+if (!function_exists('gdezakazy_is_woocommerce_active')) {
+    function gdezakazy_is_woocommerce_active() {
         $activePlugins = (array)get_option('active_plugins', array());
         if (is_multisite()) {
             $activePlugins = array_merge($activePlugins, get_site_option('active_sitewide_plugins', array()));
@@ -22,20 +22,7 @@ if (!function_exists('is_woocommerce_active')) {
     }
 }
 
-if (!function_exists('get_order_id')) {
-    function get_order_id($order) {
-        return (method_exists($order, 'get_id')) ? $order->get_id() : $order->id;
-    }
-}
-
-if (!function_exists('order_post_meta_getter')) {
-    function order_post_meta_getter($order, $attr) {
-        $meta = get_post_meta(get_order_id($order), '_'. $attr, true);
-        return $meta;
-    }
-}
-
-if (is_woocommerce_active()) {
+if (gdezakazy_is_woocommerce_active()) {
     if (!class_exists('GdeZakazy')) {
         require_once('class-gdezakazy.php');
         $GLOBALS['gdeZakazy'] = GdeZakazy::instance();
